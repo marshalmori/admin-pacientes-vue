ae879b06af97aac740129b4dc927141ff494788e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-20 11:28:00 -0300

    Criando o componente Formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,13 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="md:w-1/2">
+    <h2 class="font-black text-3xl text-center mt-10">Seguimiento Pacientes</h2>
+    <p class="text-lg mt-5 text-center mb-10">
+      Añade Pacientes y
+      <span class="text-indigo-600 font-bold">Administralos</span>
+    </p>
+  </div>
+</template>
