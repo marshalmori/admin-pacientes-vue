5d2a4f1e3b00d901cee1e13a505202bf765ba2a0
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-20 11:37:05 -0300

    Iniciando com o formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -9,5 +9,19 @@ export default {};
       Añade Pacientes y
       <span class="text-indigo-600 font-bold">Administralos</span>
     </p>
+
+    <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <div class="mb-5">
+        <label for="mascota" class="block text-gray-700 uppercase font-bold"
+          >Nombre Mascota</label
+        >
+        <input
+          id="mascota"
+          type="text"
+          placeholder="Nombre de la mascota"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+        />
+      </div>
+    </form>
   </div>
 </template>
