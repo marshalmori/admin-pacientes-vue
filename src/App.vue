8c201b289148c755390b765d91902a1ca22d91f5
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-21 16:45:17 -0300

    Inicio da lista de pacientes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script setup>
+import { ref } from "vue";
+
 import Header from "./components/Header.vue";
 import Formulario from "./components/Formulario.vue";
+
+const pacientes = ref([]);
 </script>
 
 <template>
   <div class="container mx-auto mt-20">
     <Header />
 
-    <div class="'mt-12 md:flex'">
+    <div class="'mt-12 md:flex md:flex-row">
       <Formulario />
+
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll mt-10">
+        <h3 class="font-black text-3xl text-center">
+          Administra tus Pacientes
+        </h3>
+        <div v-if="pacientes.length > 0"></div>
+        <p v-else class="text-lg mt-5 text-center mb-10">No Hay pacientes</p>
+      </div>
     </div>
   </div>
 </template>
