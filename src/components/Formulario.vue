956cf64aa8418b543f96317407f8a81af6f3fad6
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-22 10:05:35 -0300

    Mostrar uma mensagem quando o paciente for inserido com sucesso

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -47,6 +47,15 @@ const validar = () => {
   }
 
   emit("guardar-paciente");
+  alerta.mensaje = "Paciente Almacenado Correctamente";
+  alerta.tipo = "exito";
+
+  setTimeout(() => {
+    Object.assign(alerta, {
+      tipo: "",
+      mensaje: "",
+    });
+  }, 3000);
 };
 </script>
 
