5797190662eb55fa995b3cc9a0942952b7545e5e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-22 09:43:51 -0300

    Mostrar os cards com os dados dos pacientes

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref, reactive } from "vue";
 
 import Header from "./components/Header.vue";
 import Formulario from "./components/Formulario.vue";
+import Paciente from "./components/Paciente.vue";
 
 const pacientes = ref([]);
 
@@ -37,7 +38,14 @@ const guardarPaciente = () => {
         <h3 class="font-black text-3xl text-center">
           Administra tus Pacientes
         </h3>
-        <div v-if="pacientes.length > 0"></div>
+        <p class="text-lg mt-5 text-center mb-10">
+          Información de
+          <span class="text-indigo-600 font-bold">Pacientes</span>
+        </p>
+
+        <div v-if="pacientes.length > 0">
+          <Paciente v-for="paciente in pacientes" :paciente="paciente" />
+        </div>
         <p v-else class="text-lg mt-5 text-center mb-10">No Hay pacientes</p>
       </div>
     </div>
