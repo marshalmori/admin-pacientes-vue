f627c4d5523b11996597717490f805d64c06a700
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-22 10:53:00 -0300

    Carregar os dados do cliente já cadastrado no formulário para edição

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,10 @@ const guardarPaciente = () => {
 };
 
 const actualizarPaciente = (id) => {
-  console.log("actualizando...", id);
+  const pacienteEditar = pacientes.value.filter(
+    (paciente) => paciente.id === id
+  )[0];
+  Object.assign(paciente, pacienteEditar);
 };
 </script>
 
